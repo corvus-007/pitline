fc8395c2a49a582080b4a9402b17cba079034f87
Denis <corvus007@yandex.ru>
2017-02-10 17:24:35 +0300

    Доработка главной страницы

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,57 @@
+// Add it after jquery.magnific-popup.js and before first initialization code
+$.extend(true, $.magnificPopup.defaults, {
+  tClose: 'Закрыть (Esc)', // Alt text on close button
+  tLoading: 'Загрузка...', // Text that is displayed during loading. Can contain %curr% and %total% keys
+  gallery: {
+    tPrev: 'Назад (Left arrow key)', // Alt text on left arrow
+    tNext: 'Вперед (Right arrow key)', // Alt text on right arrow
+    tCounter: '%curr% из %total%' // Markup for "1 of 7" counter
+  },
+  image: {
+    tError: '<a href="%url%">The image</a> could not be loaded.' // Error message when image could not be loaded
+  },
+  ajax: {
+    tError: '<a href="%url%">The content</a> could not be loaded.' // Error message when ajax request failed
+  }
+});
+
+/*=====  End of Translating magnificPopup  ======*/
+
+
+// Загрузка карты
+function loadMapScript() {
+  var script = document.createElement("script");
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyC9a_UDjprd--E33HE4d9_S6I0yjvViR8o&callback=initializeMap";
+  document.head.appendChild(script);
+}
+
+// Инициализация карты
+function initializeMap() {
+  var locationOffice = { lat: 53.249121, lng: 34.335855 };
+
+  function createProp(defaultLocation) {
+    return {
+      center: defaultLocation,
+      zoom: 16,
+      streetViewControl: false,
+      zoomControl: true,
+      zoomControlOptions: {
+          position: google.maps.ControlPosition.RIGHT_CENTER
+      },
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false,
+    };
+  }
+
+  var mapProp = createProp(locationOffice);
+  var map = new google.maps.Map(document.getElementById("location-map"), mapProp);
+  var markerPriem = new google.maps.Marker({
+    position: locationOffice,
+    map: map,
+    title: '«PITLINE Брянск»'
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var indexSlider = document.querySelector('.index-slider');
 
@@ -7,5 +61,12 @@ document.addEventListener('DOMContentLoaded', function() {
       dots: true,
       appendArrows: '.index-slider__arrows-wrapper'
     });
+  }
+
+
+  var locationMap = document.querySelector('#location-map');
+
+  if (locationMap) {
+    loadMapScript();
   }
 });
